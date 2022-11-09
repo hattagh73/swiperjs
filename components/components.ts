7ea061c73ac3eb58e8f851392458dbda8551170f
hattagh73 <>
2022-11-09 19:49:17 +0800

    add components folder

---
+++ components/components.ts
@@ -0,0 +1,5 @@
+//* Import Icons Components
+export { default as LeftArrow } from './icons/LeftArrow';
+export { default as RightArrow } from './icons/RightArrow';
+export { default as RightArrowThin } from './icons/RightArrowThin';
+export { default as LeftArrowThin } from './icons/LeftArrowThin';
